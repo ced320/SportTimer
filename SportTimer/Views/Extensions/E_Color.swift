5c59cadd69590fcb0c12921f52572eefbb317c14
Cedric Frimmel-Hoffmann <cedric@familie-frimmel.de>
2023-07-31 15:05:19 +0200

    test

---
+++ SportTimer/Views/Extensions/E_Color.swift
@@ -0,0 +1,17 @@
+//
+//  E_Color.swift
+//  SportTimer
+//
+//  Created by Cedric Frimmel-Hoffmann on 31.07.23.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: Int, opacity: Double = 1.0) {
+        let red = Double((hex & 0xff0000) >> 16) / 255.0
+        let green = Double((hex & 0xff00) >> 8) / 255.0
+        let blue = Double((hex & 0xff) >> 0) / 255.0
+        self.init(.sRGB, red: red, green: green, blue: blue, opacity: opacity)
+    }
+}
